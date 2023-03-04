bfd1cd21e8cedecba15baed2b6a0b1b304579ce5
SoWhat <dssergeev123@gmail.com>
2023-03-04 23:50:17 +0300

    HomeWork6

---
+++ Assets/Shaders/TestShader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestShader : MonoBehaviour
+{
+    Material material;
+    private void Start()
+    {
+        material.SetColor("_Color", Color.white);
+        float height = material.GetFloat("_MixValue");
+        height = 0.5f;
+    }
+}
