1254d3f6005d033866227739a012d38b792e3187
SoWhat <dssergeev123@gmail.com>
2023-03-05 18:10:18 +0300

    HomeWork8

---
+++ Assets/Scripts/CustomRP/CameraRenderer.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+partial class CameraRenderer 
+{
+    private ScriptableRenderContext _context;
+    private Camera _camera;
+    private CommandBuffer _commandBuffer;
+    private const string _bufferName = "Camera Render";
+    private CullingResults _cullingResults;
+    private static readonly List<ShaderTagId> drawingShaderTagIds = new List<ShaderTagId> { new ShaderTagId("SRPDefaultUnlit") };
+    
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        _camera = camera;
+        _context = context;
+        UIGO();
+        if (!Cull(out var parameters))
+        {
+            return;
+        }
+
+        Settings(parameters);
+        DrawVisible();
+        DrawUnsupportedShaders();
+        DrawGizmos();
+        Submit();
+    }
+
+    private void Settings(ScriptableCullingParameters parameters)
+    {
+        _commandBuffer = new CommandBuffer { name = _camera.name };
+        _cullingResults = _context.Cull(ref parameters);
+        _context.SetupCameraProperties(_camera);
+        _commandBuffer.ClearRenderTarget(true,true,Color.clear);
+        _commandBuffer.BeginSample(_bufferName);
+        _commandBuffer.SetGlobalColor("_Global", Color.blue);
+        ExecuteCommandBuffer();
+    }
+
+    private void DrawVisible()
+    {
+        var drawingSettings = CreateDrawingSettings(drawingShaderTagIds, SortingCriteria.CommonOpaque, out var sortingSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.all);
+
+        _context.DrawRenderers(_cullingResults, ref drawingSettings, ref filteringSettings);
+        _context.DrawSkybox(_camera);
+
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+
+        _context.DrawRenderers(_cullingResults, ref drawingSettings, ref filteringSettings);
+    }
+
+    private DrawingSettings CreateDrawingSettings(List<ShaderTagId> shaderTags, SortingCriteria sortingCriteria, out SortingSettings sortingSettings)
+    {
+        sortingSettings = new SortingSettings(_camera)
+        {
+            criteria = sortingCriteria,
+        };
+
+        var drawingSettings = new DrawingSettings(shaderTags[0], sortingSettings);
+
+        for (int i = 1; i < shaderTags.Count; i++)
+        {
+            drawingSettings.SetShaderPassName(i, shaderTags[i]);
+        }
+
+        return drawingSettings;
+    }
+
+    private void Submit()
+    {
+        _commandBuffer.EndSample(_bufferName);
+        ExecuteCommandBuffer();
+        _context.Submit();
+    }
+
+    private void ExecuteCommandBuffer()
+    {
+        _context.ExecuteCommandBuffer(_commandBuffer);
+        _commandBuffer.Clear();
+    }
+
+    private bool Cull(out ScriptableCullingParameters parameters)
+    {
+        return _camera.TryGetCullingParameters(out parameters);
+    }
+
+}
+
+
+
+
---
+++ Assets/Scripts/CustomRP/SpaceRunPipelineRender.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace CustomRenderPipeline
+{
+
+    public class SpaceRunPipelineRender : RenderPipeline
+    {
+
+        private CameraRenderer _cameraRenderer;
+
+        protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+        {
+            _cameraRenderer = new CameraRenderer();
+            CamerasRender(context, cameras);
+        }
+
+        private void CamerasRender(ScriptableRenderContext context, Camera[] cameras)
+        {
+            foreach (var camera in cameras)
+            {
+                _cameraRenderer.Render(context, camera);
+            }
+        }
+    }
+}
